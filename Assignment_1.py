2964f9c1571a35dd9749f080a26344dcc8bfbf31
ishan_024 <111951184+MrVisen@users.noreply.github.com>
2023-06-03 16:30:03 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,90 @@
+#Q1. Create one variable containing following type of data:
+#(i) string----> user_1='game'
+#(ii) list------>user_2=[1,2,"ear"]
+#(iii) float------>user_3=33.98
+#(iv) tuple
+
+#Q2. Given are some following variables containing data:
+#    What will be the data type of the above given variable.
+#(i) var1 = ‘ ‘-----------------------> string
+#(ii) var2 = ‘[ DS , ML , Python]’----->string
+#(iii) var3 = [ ‘DS’ , ’ML’ , ‘Python’ ]------>list
+#(iv) var4 = 1. ------------------------------>float
+
+
+#Q3. Explain the use of the following operators using an example:
+#(i) /-----> 4/2=2
+#(ii) %-----> 4%3=1
+#(iii) //----->4//5=0
+#(iv) **------->2**3=8
+
+#Q4. Create a list of length 10 of your choice containing multiple types of data. Using for loop print the
+#element and its data type.
+  
+#user_1=[1,3,5,'lion',True,44.54,88,9.0,False,"KING"]
+#for a in user_1:
+#    print(a)
+#    print(type(a))
+
+
+#Q5. Using a while loop, verify if the number A is purely divisible by number B and if so then how many
+#    times it can be divisible.
+
+# a=[24,3,9,35,5,8,27,88,69,77,86,55,15,22,81,90,91,71,11,244,998,733,22]
+# for i in a:
+#     if(i%3==0):
+#         print("%d is divisible by 3"%(i))
+
+# for c in a:
+#     if(c%3!=0):      
+#         print("%d is not divisible by 3"%(c))
+
+
+
+# What do you understand about mutable and immutable data types? Give examples for both showing
+# this property.
+
+# Mutable data types are those that can be changed by changing the indexes and immutable are those which cannot be changed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Q6. Create a list containing 25 int type data. Using for loop and if-else condition print if the element is
+#divisible by 3 or not.
+
+
+# a=range(2,28)
+# for i in a:
+#    if(i%3==0):
+#        print(f" {i} is divisible by 3")
+#    else:
+#        print(f"{i} is not divisible by 3")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
