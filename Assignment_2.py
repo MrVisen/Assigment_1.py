41070111068ea284ffcc8bde6866ec10002ae0a4
ishan_024 <111951184+MrVisen@users.noreply.github.com>
2023-06-14 11:19:14 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,58 @@
+# Q1. How do you comment code in Python? What are the different types of comments?
+
+# We comment by # in python
+
+# Q2. What are variables in Python? How do you declare and assign values to variables?
+
+# Variables are containers that store data types.
+
+# Q3. How do you convert one data type to another in Python?
+
+# We convert one data type to another by type-casting.
+# eg: 
+# user_1="22"
+# user_2=int(user_1)
+# print(user_2)
+
+# Q5. Given a list my_list = [1, 2, 3, 4, 5], write the code to slice the list and obtain the sub-list [2, 3].
+
+# my_list = [1, 2, 3, 4, 5]
+# print(my_list[1:3])
+
+
+# Q6. What is a complex number in mathematics, and how is it represented in Python?
+
+# In python it is represented by a+bj
+
+
+# Q7. What is the correct way to declare a variable named age and assign the value 25 to it?
+
+# user_1={'age':25}
+
+# Q8. Declare a variable named price and assign the value 9.99 to it. What data type does this variable
+# belong to?
+
+# user_1={"price":9.99}
+# it belong to dict datatype
+
+# Q9. Create a variable named name and assign your full name to it as a string. How would you print the
+# value of this variable?
+
+# user_1={"name":"ishan visen"}
+# print(user_1["name"])
+
+
+# Q10. Given the string "Hello, World!", extract the substring "World".
+
+# a="Hello, World"
+# print(a[7:])
+
+
+# Q11. Create a variable named "is_student" and assign it a boolean value indicating whether you are
+# currently a student or not.
+
+# a=bool(input("is_student: "))
+# if(a==True):
+#     print("STUDENT")
+# else:
+#     print("NOT A STUDENT")
